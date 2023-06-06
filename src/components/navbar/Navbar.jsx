124de50f2a39edd1ba941c8694f3b469ad9fa01e
mlopez <ml.3012@gmail.com>
2023-06-06 15:40:10 -0300

    responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,23 @@
 import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar  from 'react-bootstrap/Navbar';
 import './navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link as ScrollLink} from 'react-scroll';
 
 const MyNavbar = () => {
 
+    const [font, setFont] = useState(false)
+
+    const darkFont = () => font === false? setFont('fontHamburguer') : setFont(false);
+
     return (
         <>
-            <Navbar className='navBar sticky-top' variant="dark" expand="lg">
-                <Container>
+            <Navbar onToggle={darkFont} className='navBar sticky-top' variant="dark" expand="lg">
+                <Container className={font}>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mx-auto">
+                        <Nav className="mx-auto btnNavBar">
                             <Nav.Link className='me-3' href="#home">Inicio</Nav.Link>
                             <Nav.Link className='me-3' href="#about">Acerca de mi</Nav.Link>
                             <Nav.Link className='me-3' href="#skills">Habilidades</Nav.Link>
