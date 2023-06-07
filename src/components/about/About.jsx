249f8e07c8594f3df341556bf63aaa40aad4e366
mlopez <ml.3012@gmail.com>
2023-06-07 14:32:48 -0300

    final

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,13 +7,11 @@ const About = () => {
         <h2>Acerca de mi</h2>
         <div className='bio'>
           <p>
-            Soy un Desarrollador Full Stack con experiencia en proyectos freelance.
-            Soy un apasionado por la tecnología y la música, y cuento con amplia experiencia en docencia.
+            Soy Desarrollador Full Stack con experiencia en proyectos freelance. 
+            Me apasiona la programación integral de aplicaciones web y me especializo en el desarrollo Frontend y Backend. Me encanta la tecnología y la música y cuento con amplia experiencia en docencia. 
             Mi objetivo es seguir creciendo en todo lo relacionado al desarrollo de software. <br/>
             Soy una persona muy organizada y metódica, me gustan los desafíos y, por sobre todo, 
             soy tenaz y perseverante en los objetivos que me propongo.
-            Me apasiona la programación integral de aplicaciones web y me especializo en el desarrollo Frontend
-            y Backend.
           </p>
         </div>
       </div>    
