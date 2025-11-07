a07945821f5257db908b616747e14c6f95f92521
mlopez <ml.3012@gmail.com>
2025-11-07 14:05:07 -0300

    update cv and component about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
         <h2>Acerca de mi</h2>
         <div className='bio'>
           <p>
-            Soy Desarrollador de Software con experiencia en análisis de datos y como tutor de cursos de programación. 
+            Soy Desarrollador de Software con experiencia en ingenieria de datos y como tutor de cursos de programación. 
             Me apasiona la programación integral de aplicaciones y el trabajo en equipo. Me encanta la tecnología y la música y cuento con amplia experiencia en docencia. 
-            Mi objetivo es seguir creciendo en todo lo relacionado al mundo de sistemas. <br/>
+            <br/>
             Soy una persona muy organizada y metódica, me gustan los desafíos y, por sobre todo, 
             soy tenaz y perseverante en los objetivos que me propongo.
           </p>
